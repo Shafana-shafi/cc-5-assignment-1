b4ce14a808a39841acaeab5198a9fe1866da2551
Shafana <shafana.shafi@codecraft.co.in>
2024-03-27 17:12:28 +0530

    implmented prime with assertions

---
+++ prime.js
@@ -0,0 +1,24 @@
+let flag=0;
+function assert(condition,message){
+    if(!flag){
+    console.log("WELCOME TO CHECK PRIME FUNCTION")
+    }
+   if(!condition){
+    console.assert(message)
+    throw(message);
+   }
+   flag=1;
+}
+function checkPrime(a){
+    for(let i=2;i<=Math.sqrt(a);i++){
+        if(a%i===0)
+          return false;
+        }console.log("Is a prime number");
+        return true;
+}
+function prime(a){
+    assert(typeof a ==='number',"Argument is not a number");
+    assert(a>1,"Is not a prime number");
+    assert(checkPrime(a),"Is not a prime number");
+  }
+prime(11);
