78d338ba3f399710c1aef3ce35659336e0da7447
Shafana <shafana.shafi@codecraft.co.in>
2024-04-01 09:06:46 +0530

    co located asserts near to its functions

--- prime.js
+++ prime.js
@@ -11,6 +11,15 @@ function isPrime(num) {
   }
   return true;
 }
+// Post-conditions positive conditions for isPrime()
+assert(isPrime(2), "Is not a prime Number");
+assert(isPrime(3), "Is not a prime Number");
+assert(isPrime(4) === false, "Is a prime number");
+assert(isPrime(9) === false, "Is a prime number");
+assert(isPrime(29),"Is not a prime number")
+//negative conditions that throws exception
+assert(isPrime(-3));
+assert(isPrime(" "));
 //This is a function to generate specific number of  prime numbers staring from 2
 function generatePrimeSeries(count){
   assert(typeof count === 'number', "Argument is not a number");
@@ -25,15 +34,6 @@ function generatePrimeSeries(count){
   }
   return primesNumbers;
 }
-// Post-conditions positive conditions for isPrime()
-assert(isPrime(2), "Is not a prime Number");
-assert(isPrime(3), "Is not a prime Number");
-assert(isPrime(4) === false, "Is a prime number");
-assert(isPrime(9) === false, "Is a prime number");
-assert(isPrime(29),"Is not a prime number")
-//negative conditions that throws exception
-assert(isPrime(-3));
-assert(isPrime(" "));
 //post conditions for generatePrimeSeries
 assert.deepStrictEqual(generatePrimeSeries(5),[2,3,5,7,11],"The output of generatePrimeSeries is in correct");
 assert.deepStrictEqual(generatePrimeSeries(3),[2,5,7,9,11],"The output of generatePrimeSeries is in correct");
