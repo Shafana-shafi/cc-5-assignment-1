4e02b734e42f8492afccb9743c07b632ae63ece7
“shafana” <shafana.shafi@codecraft.co.in>
2024-04-08 09:40:40 +0530

    added jsdoc and cahnged the parameter name from data to array

--- linkedlist.js
+++ linkedlist.js
@@ -1,7 +1,12 @@
-export function createList(data = null) {
-    if (Array.isArray(data)) {
+/**
+ * Creates a new linked list from the given array or returns an empty list if no array is provided.
+ * @param {Array} [array=null] The array of elements to be added to the linked list. If not provided, an empty list is returned.
+ * @returns {Object} An object representing the linked list.If the input is not an array, an empty list object is returned.
+ */
+export function createList(array = null) {
+    if (Array.isArray(array)) {
         let list = { head: null, tail: null }
-        data.forEach(element => {
+        array.forEach(element => {
             let newNode = { data: element, next: null }
             if (list.head === null) {
                 list.head = newNode;
@@ -18,7 +23,12 @@ export function createList(data = null) {
         return { head: null, tail: null };
     }
 }
-
+/**
+ * Adds a new node with data to the end of a linked list.
+ * @param {Object} listRef The linked list object.
+ * @param {*} data The data for the new node.
+ * @returns {Object} The new node added to the list.
+ */
 export function addItemsToList(listRef, data) {
     let newNode = { data: data, next: null };
     if (listRef.head === null) {
@@ -30,7 +40,11 @@ export function addItemsToList(listRef, data) {
     }
     return newNode;
 }
-
+/**
+ * Converts an array to a linked list.
+ * @param {Array} array The input array.
+ * @returns {Object} The linked list created from the array.
+ */
 export function listFromArray(array) {
     let list = createList();
     for (let i = 0; i < array.length; i++) {
@@ -38,6 +52,13 @@ export function listFromArray(array) {
     }
     return list;
 }
+/**
+ * Inserts a new node with newData before the first node containing targetData.
+ * @param {Object} listRef The linked list object.
+ * @param {*} targetData The data to find in the list.
+ * @param {*} newData The data for the new node.
+ * @returns {boolean} True if the node was inserted, false otherwise.
+ */
 export function insertBefore(listRef, targetData, newData) {
     let currentNode = listRef.head;
     let prevNode = null;
@@ -57,6 +78,11 @@ export function insertBefore(listRef, targetData, newData) {
     }
     return false;
 }
+/**
+ * Removes the last node from the list.
+ * @param {Object} listRef The linked list object.
+ * @returns {boolean} True if the node was removed, false if the list is empty.
+ */
 export function removeLast(listRef) {
     if (listRef.head === null) {
         return false;
@@ -74,9 +100,13 @@ export function removeLast(listRef) {
     listRef.tail = cur;
     return true;
 }
-
-
-
+/**
+ * Inserts a new node with newData after the first node containing targetData.
+ * @param {Object} listRef The linked list object.
+ * @param {*} targetData The data to find in the list.
+ * @param {*} newData The data for the new node.
+ * @returns {boolean} True if the node was inserted, false otherwise.
+ */
 export function insertAfter(listRef, targetData, newData) {
     let currentNode = listRef.head;
     while (currentNode !== null) {
@@ -92,7 +122,11 @@ export function insertAfter(listRef, targetData, newData) {
     }
     return false;
 }
-
+/**
+ * Converts a linked list to an array.
+ * @param {Object} list The linked list object.
+ * @returns {Array} An array containing the data from each node in the list.
+ */
 export function listToArray(list) {
     const array = [];
     let currentNode = list.head;
@@ -102,6 +136,12 @@ export function listToArray(list) {
     }
     return array;
 }
+/**
+ * Filters the list based on a condition and returns an array of the filtered data.
+ * @param {Object} listRef The linked list object.
+ * @param {Function} [isString=(node)=>{}] A function to test each node's data. Defaults to checking if the data is a string.
+ * @returns {Array} An array of the filtered data.
+ */
 export function filterList(listRef, isString = (node) => {
     if (typeof node.data === 'string') {
         return true;
@@ -117,8 +157,13 @@ export function filterList(listRef, isString = (node) => {
         cur = cur.next;
     }
     return result;
-
 }
+/**
+ * Removes the first node containing the specified item from the list.
+ * @param {Object} listRef The linked list object.
+ * @param {*} item The data to remove from the list.
+ * @returns {boolean} True if the node was removed, false otherwise.
+ */
 export function removeItem(listRef, item) {
     let currentNode = listRef.head;
     let prevNode = null;
@@ -143,6 +188,11 @@ export function removeItem(listRef, item) {
     }
     return false;
 }
+/**
+ * Creates a new linked list from the given data.
+ * @param {Object} data The data to create the new list from. Must have `head` and `tail` properties.
+ * @returns {Object|null} The new linked list object or null if the input data is invalid.
+ */
 export function createNewList(data) {
     if (!(data.head && data.tail)) {
         console.log(data);
